d3bf59a2b96d2a0bf8ff34175c2ec4b9e8e6c47c
Gabriel Rosser <gabriel.rosser@gmail.com>
2017-04-29 10:56:41 +0100

    Continued with parsing. Added house types. Full Description proving tricky

--- api/parser.py
+++ api/parser.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+from django.contrib.gis import geos
 from bs4 import BeautifulSoup
 import re
 import consts
@@ -14,7 +16,7 @@ BUILDING_TYPE_MAP = {
     'flat': consts.BUILDING_TYPE_FLAT,
     'apartment': consts.BUILDING_TYPE_FLAT,
     'maisonette': consts.BUILDING_TYPE_MAISONETTE,
-    'house': consts.BUILDING_TYPE_MAISONETTE,
+    'house': consts.BUILDING_TYPE_HOUSE,
     'retirement property': consts.BUILDING_TYPE_RETIREMENT,
     'town house': consts.BUILDING_TYPE_TOWNHOUSE,
     'country house': consts.BUILDING_TYPE_COUNTRYHOUSE,
@@ -22,12 +24,20 @@ BUILDING_TYPE_MAP = {
     'penthouse': consts.BUILDING_TYPE_PENTHOUSE,
 }
 
-BUILDING_SITUATION_MAP = dict([
-    (v.lower(), k) for k, v in dict(consts.BUILDING_SITUATION_CHOICES).items()
-])
+
+BUILDING_SITUATION_MAP = {
+"detached": consts.BUILDING_SITUATION_DETACHED,
+"semi-detached": consts.BUILDING_SITUATION_SEMIDETACHED,
+"end of terrace": consts.BUILDING_SITUATION_ENDTERRACE,
+"terraced": consts.BUILDING_SITUATION_MIDTERRACE,
+"link detached": consts.BUILDING_SITUATION_LINKDETACHED,
+"ground floor": consts.BUILDING_SITUATION_GROUNDFLOOR,
+}
 
 situation_re = re.compile("(?P<t>%s)" % "|".join(BUILDING_SITUATION_MAP.keys()), flags=re.I)
 type_re = re.compile("(?P<t>%s)" % "|".join(BUILDING_TYPE_MAP.keys()), flags=re.I)
+latlng_re = re.compile(r"latitude=(?P<lat>[-0-9\.]*).*longitude=(?P<lng>[-0-9\.]*)")
+
 
 def residential_property_for_sale(src):
     # filter out unwanted items here
@@ -52,7 +62,7 @@ def residential_property_for_sale(src):
 
     # attributes
     x = soup.find(attrs={'class': 'property-header-bedroom-and-price'})
-    desc = x.h1
+    desc = x.find('h1')
 
     if desc is None:
         # without description, we won't have enough to go on
@@ -81,15 +91,66 @@ def residential_property_for_sale(src):
     # we now seek to extract up to two elements: situation (e.g. 'detached') and type (e.g. penthouse)
     # the second is guaranteed, the first is optional
 
+    sit = re.search(situation_re, prop)
+    if sit:
+        t = sit.group('t').lower()
+        if t in BUILDING_SITUATION_MAP:
+            res['building_situation'] = BUILDING_SITUATION_MAP[t]
+        else:
+            res.setdefault('errors', {})['building_situation'] = t
+        prop2 = re.sub(situation_re, "", prop).strip()
+    else:
+        prop2 = prop
+
+    typ = re.search(type_re, prop2)
+    if typ:
+        t = typ.group('t').lower()
+        if t in BUILDING_TYPE_MAP:
+            res['building_type'] = BUILDING_TYPE_MAP[t]
+            if res['building_type'] == consts.BUILDING_TYPE_FLAT:
+                res['building_situation'] = consts.BUILDING_SITUATION_FLAT
+        else:
+            res.setdefault('errors', {})['building_type'] = t
+    else:
+        res.setdefault('errors', {})['building_type'] = prop
+
+    # description
+    desc = soup.find(attrs={'id': 'description'})
+    res['key_features'] = [t.text for t in desc.find_all('li')]
     
 
 
 
-
-
-
-
-
-
-    # description
     # location
+    res['address_string'] = x.find('address')
+    map_static = soup.find(attrs={'alt': 'Get map and local information'})
+    if map_static is None:
+        res.setdefault('errors', {})['location'] = 'not found'
+    else:
+        t = map_static.get('src')
+        latlng = re.search(latlng_re, t)
+        if latlng is None:
+            res.setdefault('errors', {})['location'] = t
+        else:
+            try:
+                lat = float(latlng.group('lat'))
+                lng = float(latlng.group('lng'))
+                res['location'] = geos.Point(lng, lat, srid=4326)
+            except ValueError:
+                res.setdefault('errors', {})['location'] = t
+
+    # asking price
+    prc = soup.find(attrs={'class': 'property-header-price'})
+    if prc is None:
+        res.setdefault('errors', {})['asking_price'] = 'not found'
+    else:
+        price = re.search(u'£(?P<price>[0-9,]*)', prc.text).group('price')
+        try:
+            res['asking_price'] = int(price.replace(',', ''))
+        except ValueError:
+            res.setdefault('errors', {})['asking_price'] = price
+
+    # status
+    stat = soup.find(attrs={'class': 'property-header-qualifier'})
+    if stat is not None:
+        res['qualifier'] = stat.text
