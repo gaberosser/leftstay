6e89a348627133fb6a9c4196fb3a2b26455ede21
Gabriel Rosser <gabriel.rosser@gmail.com>
2017-05-21 08:41:16 +0100

    Restart celery worker/beat if required

--- web/run_celery.sh
+++ web/run_celery.sh
@@ -3,6 +3,8 @@
 # wait for RabbitMQ server to start
 sleep 4
 
+
+
 # run Celery worker with Celery configuration stored in celeryconf
 # we're disabling concurrency for this worker because it will be used to make HTTP requests
 su -m myuser -c "celery worker -A celerystick.conf -Q default -n default@%h -l INFO --concurrency=1"
