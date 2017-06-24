8b4b82eedf46cb575a545596e0cb6eda40d62be1
Gabriel Rosser <gabriel.rosser@gmail.com>
2017-06-24 06:32:42 +0100

    Tidy up author names

--- web/bibliography/serializers.py
+++ web/bibliography/serializers.py
@@ -3,22 +3,33 @@ from . import models
 
 
 class PublicationAuthorshipSerializer(serializers.ModelSerializer):
-    first_name = serializers.ReadOnlyField(source="author.first_name")
-    last_name = serializers.ReadOnlyField(source="author.last_name")
+    # first_name = serializers.ReadOnlyField(source="author.first_name")
+    # last_name = serializers.ReadOnlyField(source="author.last_name")
+    full_name = serializers.SerializerMethodField()
     bold = serializers.ReadOnlyField(source="author.make_bold")
 
     class Meta:
         model = models.PublicationAuthorship
         fields = (
-            'first_name',
-            'last_name',
-            'order',
+            'full_name',
+            # 'first_name',
+            # 'last_name',
+            # 'order',
             'bold',
         )
 
+    def get_full_name(self, obj):
+        ln = obj.author.last_name
+        init = [t[0].upper() for t in obj.author.first_name.split()]
+        if obj.author.middle_initials is not None:
+            init += [t[0].upper() for t in obj.author.middle_initials]
+        init = ''.join("%s. " % t for t in init).strip()
+        return "%s, %s" % (ln, init)
+
 
 class PublicationSerializer(serializers.ModelSerializer):
-    authors = PublicationAuthorshipSerializer(source='publicationauthorship_set', many=True, read_only=True)
+    authors = serializers.SerializerMethodField()
+    title = serializers.SerializerMethodField()
 
     class Meta:
         model = models.Publication
@@ -33,3 +44,12 @@ class PublicationSerializer(serializers.ModelSerializer):
             'page_end',
         )
 
+    def get_title(self, obj):
+        ttl = obj.title.strip()
+        if ttl[-1] not in {'.', '?', '!'}:
+            ttl += '.'
+        return ttl
+
+    def get_authors(self, obj):
+        ordered_qset = obj.publicationauthorship_set.order_by('order')
+        return PublicationAuthorshipSerializer(ordered_qset, many=True).data
